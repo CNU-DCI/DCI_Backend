d8239e8de3e38a2a3c7338ee6627302e39132bb5
0-tae <choiyt3465@naver.com>
2023-05-21 21:12:28 +0900

    init

---
+++ src/main/java/dciproject/backend/DciProjectApplication.java
@@ -0,0 +1,13 @@
+package dciproject.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DciProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DciProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dciproject/backend/DciProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dciproject.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DciProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
